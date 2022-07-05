b50c7119040c4a64370211ce0531e959711331ea
DanGoer <87978392+DanGoer@users.noreply.github.com>
2022-07-05 16:05:58 +0200

    added animated Footer with FM

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,12 +1,12 @@
 import { useState } from "react";
-import { motion } from "framer-motion";
+import { motion, AnimatePresence } from "framer-motion";
 
 function Footer() {
-  const [isOpen, setIsOpen] = useState(true);
+  const [isOpen, setIsOpen] = useState(false);
 
   const rocket = {
     start: {
-      y: -200,
+      y: -100,
       transition: { duration: 1, ease: "easeIn" },
     },
     landing: {
@@ -30,50 +30,61 @@ function Footer() {
   };
 
   return (
-    <footer
-      className="w-full h-24 bg-gradient-to-r from-card to-text 
-    flex flex-col items-center justify-center absolute bottom-0 gap-2"
-    >
-      <h5 className="text-md">
-        This App was created by{" "}
-        <a
-          href="https://www.linkedin.com"
-          target="_blank"
-          rel="noopener noreferrer"
-          className="text-para hover:text-but"
-        >
-          {" "}
-          D. Goergens
-        </a>{" "}
-      </h5>
-      <div className="flex flex-col md:flex-row gap-1 text-xs">
-        <div className="font-bold ">
-          A challenge from{" "}
-          <a
-            href="https://www.frontendmentor.io/home"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="text-para hover:text-but"
+    <footer>
+      <AnimatePresence>
+        {isOpen && (
+          <motion.div
+            initial={{ height: 0 }}
+            animate={{ height: 100 }}
+            transition={{ duration: 1, ease: "easeIn" }}
+            exit={{ height: 0 }}
+            className="w-full h-24 bg-gradient-to-r over from-green-300 to-white 
+         flex-col items-center justify-center absolute bottom-0 gap-2 flex"
           >
-            {" "}
-            Frontend Mentor
-          </a>
-          .
-        </div>{" "}
-        <div>
-          The code can be found here:{" "}
-          <a
-            href="https://github.com/"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="text-para hover:text-but"
-          >
-            {" "}
-            Github
-          </a>
-        </div>
-      </div>
+            <h5 className="text-md">
+              This App was created by{" "}
+              <a
+                href="https://www.linkedin.com"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="text-para hover:text-but"
+              >
+                {" "}
+                D. Goergens
+              </a>{" "}
+            </h5>
+            <div className="flex flex-col md:flex-row gap-1 text-xs">
+              <div className="font-bold ">
+                A challenge from{" "}
+                <a
+                  href="https://www.frontendmentor.io/home"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="text-para hover:text-but"
+                >
+                  {" "}
+                  Frontend Mentor
+                </a>
+                .
+              </div>{" "}
+              <div>
+                The code can be found here:{" "}
+                <a
+                  href="https://github.com/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="text-para hover:text-but"
+                >
+                  {" "}
+                  Github
+                </a>
+              </div>
+            </div>
+          </motion.div>
+        )}
+      </AnimatePresence>
       <motion.button
+        className="absolute right-3 bottom-1"
         onClick={() => setIsOpen(!isOpen)}
         variants={rocket}
         animate={isOpen ? "start" : "landing"}
