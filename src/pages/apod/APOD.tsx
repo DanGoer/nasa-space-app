02805b73896dd42e4632c8cf8d9fb4940e936f26
DanGoer <87978392+DanGoer@users.noreply.github.com>
2022-07-10 22:04:55 +0200

    reworked api call and displayed first parts of data

--- src/pages/apod/APOD.tsx
+++ src/pages/apod/APOD.tsx
@@ -1,24 +1,43 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
+import GenericCard from "../../components/GenericCard";
 
 function APOD() {
-  const [aPod, setApod] = useState([]);
+  const [aPod, setApod] = useState<any>(null);
 
   const API_KEY = process.env.REACT_APP_APOD_API_KEY;
 
+  const getApod = async () => {
+    const res: any = await axios.get(
+      `https://api.nasa.gov/planetary/apod?api_key=${API_KEY}&count=5`
+    );
+    setApod(res);
+  };
+
   useEffect(() => {
-    const getApod = async () => {
-      const res: any = await axios.get(
-        `https://api.nasa.gov/planetary/apod?api_key=${API_KEY}&count=5`
-      );
-      setApod(res);
-    };
     getApod();
-    console.log(JSON.stringify(aPod));
   }, []);
 
   console.log(JSON.stringify(aPod));
 
-  return <div>APOD</div>;
+  return (
+    <>
+      <h1>APOD</h1>
+      <h2>Astronomic Picture of the day</h2>
+      <GenericCard added="mt-40">
+        {aPod ? (
+          <span>
+            <h3>{aPod.data[0].title}</h3>
+            <h4>Author: {aPod.data[0].copyright}</h4>
+            <h4>Date: {aPod.data[0].date}</h4>
+            <img src={aPod.data[0].url} alt="Astronomic pictur of the day" />
+            <p>{aPod.data[0].explanation}</p>
+          </span>
+        ) : (
+          <div></div>
+        )}
+      </GenericCard>
+    </>
+  );
 }
 export default APOD;
