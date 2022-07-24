2ff4ae3811b9403dd2266288c7b3d90d83bba72e
DanGoer <87978392+DanGoer@users.noreply.github.com>
2022-07-24 13:41:46 +0200

    added asteroids

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import Footer from "./components/footer/Footer";
 
 import NavBar from "./components/navbar/NavBar";
 import APOD from "./pages/apod/APOD";
+import Asteroids from "./pages/asteroids/Asteroids";
 import Features from "./pages/features/Features";
 import Home from "./pages/home/Home";
 import Impressum from "./pages/impressum/Impressum";
@@ -18,6 +19,7 @@ function App() {
           <Route path="/impressum" element={<Impressum />} />
           <Route path="/apod" element={<APOD />} />
           <Route path="/features" element={<Features />} />
+          <Route path="/asteroids" element={<Asteroids />} />
         </Routes>
       </BackGround>
       <Footer />
--- src/pages/features/components/FeaturesCard.tsx
+++ src/pages/features/components/FeaturesCard.tsx
@@ -1,30 +1,34 @@
+import { useTranslation } from "react-i18next";
 import { Link } from "react-router-dom";
 import { featurescontent } from "../../../assets/data";
 
 function FeaturesCard() {
+  const { t } = useTranslation("featurescard");
   return (
     <div className="flex flex-col md:flex-row gap-4 md:px-4 mt-6 dark:shadow-darkOutline shadow-lightOutline shadow-2xl">
-      {featurescontent.map((card) => {
-        return (
-          <Link
-            to={card.url}
-            key={card.id}
-            className="flex flex-col min-w-[320px] 
+      {t("featurescard", { joinArrays: "+" })
+        .split("+")
+        .map((text, index) => {
+          return (
+            <Link
+              to={featurescontent[index].url}
+              key={featurescontent[index].id}
+              className="flex flex-col min-w-[320px] 
             items-center px-4 md:px-12 rounded-xl shadow-lg bg-secondaryLight dark:bg-secondaryDark text-center gap-6 py-6 transition-all hover:scale-105 duration-500"
-          >
-            <h4 className="">{card.title}</h4>
-            <p className="">{card.text}</p>
-            <svg
-              className="w-52 mx-4 mb-1 fill-backgroundLightOn dark:fill-backgroundDarkOn bg-backgroundLight dark:bg-backgroundDark"
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox={card.view}
-              fill="currentColor"
             >
-              <path d={card.path}></path>
-            </svg>
-          </Link>
-        );
-      })}
+              <h4 className="">{"features-card"}</h4>
+              <p className="">{"features-card"}</p>
+              <svg
+                className="w-52 mx-4 mb-1 fill-backgroundLightOn dark:fill-backgroundDarkOn bg-backgroundLight dark:bg-backgroundDark"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox={featurescontent[index].view}
+                fill="currentColor"
+              >
+                <path d={featurescontent[index].path}></path>
+              </svg>
+            </Link>
+          );
+        })}
     </div>
   );
 }
